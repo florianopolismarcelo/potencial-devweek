982722ddfd29ef9908885bb08a0cfe560a56ed03
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-09-03 13:56:26 -0300

    PotencialDevWeek

--- Api-pottencial-dev-week/src/Models/Person.cs
+++ Api-pottencial-dev-week/src/Models/Person.cs
@@ -2,6 +2,11 @@ namespace src.Models;
 
 public class Person
 {
+    public Person()
+    {
+        this.nome = "template";
+        this.idade = 0;
+    }
     //nome, idade, cpf, ativa
     public string nome { get; set; }
     public int idade { get; set; }
