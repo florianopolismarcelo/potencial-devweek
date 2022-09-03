aad34dd5538cfe30f9132274ae6e763d37faebde
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-09-03 18:47:15 -0300

    PotencialDevWeek

--- Api-pottencial-dev-week/src/Models/Contrato.cs
+++ Api-pottencial-dev-week/src/Models/Contrato.cs
@@ -2,5 +2,7 @@ namespace src.Models;
 
 public class Contrato
 {
-    
+    this.DataCriacao = DateTime.Now();
 }
+    public DateTime DataCriacao { get; set;}
+    public string tokenId {get; set;}
