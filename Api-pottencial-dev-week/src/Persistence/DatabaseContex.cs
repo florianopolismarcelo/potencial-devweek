937faf3583441c2d6a11e3f370605283e07cd737
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-09-08 10:49:29 -0300

    PotencialDevWeek

--- Api-pottencial-dev-week/src/Persistence/DatabaseContex.cs
+++ Api-pottencial-dev-week/src/Persistence/DatabaseContex.cs
@@ -5,6 +5,10 @@ namespace src.Persistence;
 
 public class DatabaseContext : DbContext
 {
+    public DatabaseContex(DbContextOptions<DatabaseContext> options)
+    {
+        
+    }
     public int DbSet<Pessoa> Pessoas { get; set; }
 
     public int DbSet<Contrato> Contratos { get; set; }
