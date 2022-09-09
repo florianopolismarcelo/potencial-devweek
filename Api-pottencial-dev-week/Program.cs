5f61c7dd72ca44cabc0fd3cc489e2d83d20964ce
florianopolismarcelo <florianopolismarcelo@msn.com>
2022-09-09 14:06:17 -0300

    PotencialDevWeek

--- Api-pottencial-dev-week/Program.cs
+++ Api-pottencial-dev-week/Program.cs
@@ -1,6 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using src.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext <DatabaseContex>
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
